4ab69b145c540053c3c21597df57254431d29a3f
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2026-06-19 18:34:24 -0300

    first commit

---
+++ backend/service/video-service/src/test/java/com/totex/miniyt/video_service/VideoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totex.miniyt.video_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
