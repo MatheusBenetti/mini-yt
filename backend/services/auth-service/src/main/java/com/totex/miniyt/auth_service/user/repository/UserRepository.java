234b31af28e37d75d6d695915fdf83e6374c641f
Matheus Bazei Benetti <mbbenetti1@gmail.com>
2026-06-23 13:42:05 -0300

    adjust User Repository to receive UUID instead of Long

--- backend/services/auth-service/src/main/java/com/totex/miniyt/auth_service/user/repository/UserRepository.java
+++ backend/services/auth-service/src/main/java/com/totex/miniyt/auth_service/user/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.totex.miniyt.auth_service.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<UUID, User> {
     Optional<User> findByEmailIgnoreCase(String email);
     boolean existsByEmailIgnoreCase(String email);
 }
